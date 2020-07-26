d8f29944ad6bc795dfb3f819b4711a0d0136bfb6
Clannis <Dodds1990@gmail.com>
2020-07-26 14:19:41 -0500

    add description to tricks table

--- db/migrate/20200726161218_create_tricks.rb
+++ db/migrate/20200726161218_create_tricks.rb
@@ -2,6 +2,7 @@ class CreateTricks < ActiveRecord::Migration[6.0]
   def change
     create_table :tricks do |t|
       t.string :name
+      t.text :description
       t.integer :difficulty_rating
 
       t.timestamps
