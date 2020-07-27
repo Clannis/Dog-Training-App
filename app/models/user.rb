9a0e879135a3d75b9c0aacd5d7266ed9494710ea
Clannis <dodds1990@gmail.com>
2020-07-27 14:48:25 -0500

    add has secure password to users and create user model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :dogs
     has_many :training_sessions, through: :dogs
     has_many :courses, through: :training_sessions
+    has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {
+    User.create(
+      :username => "MindyPoo",
+      :first_name => "Mindy",
+      :last_name => "Stevens",
+      :password => "password",
+      :phone_number => "555-555-5555",
+      :email => "Mindy@test.com"
+    )
+  }
+
+  it "is valid with a username, first and last name, password, phone number, and email" do
+    expect(user).to be_valid
+  end
+
+  it "is not valid without a password" do
+    expect(User.new(username: "Name")).not_to be_valid
+  end
+
 end
