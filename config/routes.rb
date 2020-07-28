1dc8695f34ca4c351be8857991225395ce3b1b1c
Clannis <Dodds1990@gmail.com>
2020-07-28 17:24:08 -0500

    add signup feature tests

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   resources :dogs
   resources :users
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-  root 'welcome#home'
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  get '/signup', to: 'user#new'
+  post '/signup', to: 'user#create'
+  root 'welcome#welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
