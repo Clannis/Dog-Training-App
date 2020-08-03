cda51d8ac4ee1ab3dc2318bbec460c80d2fc075a
Clannis <dodds1990@gmail.com>
2020-08-03 14:28:30 -0500

    update associations for course model to include tricks through trick_courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
-    has_many :tricks
+    has_many :trick_courses
+    has_many :tricks, through: :trick_courses
     has_many :training_sessions
     has_many :dogs, through: :training_sessions
     has_many :users, through: :dogs
