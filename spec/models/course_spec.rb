cf4bcb5413d5704ea9fb96441330af8beeca305c
Clannis <dodds1990@gmail.com>
2020-08-03 14:26:47 -0500

    add course model spec tests

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Course, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:course) {
+    Course.create(
+      name: "Puppy Beginner",
+      length: 8,
+      cost: 250
+    )
+  }
+
+  it "is valid with a name, length, and cost" do
+    expect(course).to be_valid
+  end
+
+  it "has many tricks" do
+    trick = Trick.create(name: "sit", description: "Dog sits down", difficulty_rating: 1)
+    course.tricks << trick
+    expect(course.tricks.first).to eq(trick)
+  end
 end
