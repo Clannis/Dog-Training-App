65f2b83e390b9af4d2170f9839adb82744c6a8d9
Clannis <Dodds1990@gmail.com>
2020-08-03 21:05:13 -0500

    add spec tests for trick and trick_course models

--- spec/models/trick_course_spec.rb
+++ spec/models/trick_course_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe TrickCourse, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:trick) {
+    Trick.create(
+      name: "Sit",
+      description: "The dog sits down.",
+      difficulty_rating: 1
+    )
+  }
+
+  let(:course) {
+    Course.create(
+      name: "Puppy Beginner", 
+      length: 8, 
+      cost: 250
+    )
+  }
+
+  let(:trick_course) {
+    TrickCourse.create(
+      trick_id: trick.id,
+      course_id: course.id
+    )
+  }
+
+  it "is valid with a trick_id and a course_id" do
+    expect(trick_course). to be_valid
+  end
+  
+  it "belongs to one trick" do
+    expect(trick_course.trick).to eq(trick)
+  end
+
+  it "belongs to one course" do
+    expect(trick_course.course).to eq(course)
+  end
 end
--- spec/models/trick_spec.rb
+++ spec/models/trick_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Trick, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:trick) {
+    Trick.create(
+      name: "Sit",
+      description: "The dog sits down.",
+      difficulty_rating: 1
+    )
+  }
+
+  it "is valid with a name, description, and difficulty_rating" do
+    expect(trick).to be_valid
+  end
+
+  it "has many courses through trick_courses" do
+    course = Course.create(name: "Puppy Beginner", length: 8, cost: 250)
+    trick.courses << course
+    expect(trick.courses.first).to eq(course)
+  end
 end
