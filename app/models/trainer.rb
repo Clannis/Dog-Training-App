15ae4b9529f6d4f0eea84ff3902adb5190d1d646
Clannis <Dodds1990@gmail.com>
2020-08-03 20:38:52 -0500

    add spec tests for trainer model and correct typo in trainer model

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,7 +1,7 @@
 class Trainer < ApplicationRecord
-    has_many :training_ssessions
-    has_many :courses, through: :training_ssessions
-    has_many :dogs, through: :training_ssessions
+    has_many :training_sessions
+    has_many :courses, through: :training_sessions
+    has_many :dogs, through: :training_sessions
     has_many :users, through: :dogs
 
     has_secure_password
--- spec/models/trainer_spec.rb
+++ spec/models/trainer_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Trainer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:trainer) {
+    Trainer.create(
+      username: "WD_Train", 
+      first_name: "Walt", 
+      last_name: "Disney", 
+      phone_number: "555-555-5556", 
+      email: "Disney@test.com", 
+      certification: "Top Dog Academy", 
+      password: "password"
+    )
+  }
+
+  it "is valid with a username, first and last name, phone number, email, certification, and password" do
+    expect(trainer).to be_valid
+  end
+
+  it "has many training sessions" do
+    course = Course.create(name: "Puppy Beginner", length: 8, cost: 250)
+    trainer = Trainer.create(username: "WD_Train", first_name: "Walt", last_name: "Disney", phone_number: "555-555-5556", email: "Disney@test.com", certification: "Top Dog Academy", password: "password")
+    training_session = TrainingSession.create(starts_at: Time.current, location: "Fischer Park", trainer_id: trainer.id, course_id: course.id)
+    trainer.training_sessions << training_session
+    expect(trainer.training_sessions.first).to eq(training_session)
+  end
+
+  it "has many courses through training sessions" do
+    course = Course.create(name: "Puppy Beginner", length: 8, cost: 250)
+    trainer = Trainer.create(username: "WD_Train", first_name: "Walt", last_name: "Disney", phone_number: "555-555-5556", email: "Disney@test.com", certification: "Top Dog Academy", password: "password")
+    training_session = TrainingSession.create(starts_at: Time.current, location: "Fischer Park", trainer_id: trainer.id, course_id: course.id)
+    trainer.training_sessions << training_session
+    expect(trainer.courses.first).to eq(course)
+  end
+
+  it "has many dogs through training session dogs" do
+    course = Course.create(name: "Puppy Beginner", length: 8, cost: 250)
+    trainer = Trainer.create(username: "WD_Train", first_name: "Walt", last_name: "Disney", phone_number: "555-555-5556", email: "Disney@test.com", certification: "Top Dog Academy", password: "password")
+    training_session = TrainingSession.create(starts_at: Time.current, location: "Fischer Park", trainer_id: trainer.id, course_id: course.id)
+    user = User.create(username: "Alex", first_name: "Alex", last_name: "Rodriguez", phone_number: "123-456-7890", email: "ARod@test.com", password: "password")
+    dog = Dog.create(name: "Pitty", breed: "pitbull", age: 2, shots: true, last_shot_date: Time.current, user_id: user.id)
+    training_session.dogs << dog
+    trainer.training_sessions << training_session
+    expect(trainer.dogs.first).to eq(dog)
+  end
+
+  it "has many users through dogs" do
+    course = Course.create(name: "Puppy Beginner", length: 8, cost: 250)
+    trainer = Trainer.create(username: "WD_Train", first_name: "Walt", last_name: "Disney", phone_number: "555-555-5556", email: "Disney@test.com", certification: "Top Dog Academy", password: "password")
+    training_session = TrainingSession.create(starts_at: Time.current, location: "Fischer Park", trainer_id: trainer.id, course_id: course.id)
+    user = User.create(username: "Alex", first_name: "Alex", last_name: "Rodriguez", phone_number: "123-456-7890", email: "ARod@test.com", password: "password")
+    dog = Dog.create(name: "Pitty", breed: "pitbull", age: 2, shots: true, last_shot_date: Time.current, user_id: user.id)
+    training_session.dogs << dog
+    trainer.training_sessions << training_session
+    expect(trainer.users.first).to eq(user)
+  end
 end
