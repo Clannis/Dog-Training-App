c2ccb2deed12012ba5203e572ba97e537ccd6643
Clannis <Dodds1990@gmail.com>
2020-08-03 23:57:43 -0500

    add user show page and instance method for displaying full name for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
     has_many :training_sessions, through: :dogs
     has_many :courses, through: :training_sessions
     has_secure_password
+
+    def name
+        self.first_name.downcase.capitalize + " " + self.last_name.downcase.capitalize
+    end
 end
