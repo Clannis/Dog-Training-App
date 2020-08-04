bb4b2049131a2de7f235728ee3cffd594cfda43c
Clannis <Dodds1990@gmail.com>
2020-08-04 00:22:31 -0500

    new and create methods with corresponding view for dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,21 @@
 class DogsController < ApplicationController
+    def new
+        @dog = Dog.new()
+    end
+
+    def create
+        @dog = Dog.new(dog_params)
+        byebug
+        if @dog.save
+            redirect_to user_path(User.find(id: session[:user_id]))
+        else
+            redirect_to new_dog_path
+        end
+    end
+
+    private
+
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age, :last_shot_date)
+    end
 end
