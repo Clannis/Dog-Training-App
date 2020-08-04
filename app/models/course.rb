a06f1fa909208c9f487fd7f49d3162a0685eacdc
Clannis <Dodds1990@gmail.com>
2020-08-03 19:43:59 -0500

    update associations for course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,7 @@ class Course < ApplicationRecord
     has_many :trick_courses
     has_many :tricks, through: :trick_courses
     has_many :training_sessions
-    has_many :dogs, through: :training_sessions
+    has_many :training_session_dogs, through: :training_sessions
+    has_many :dogs, through: :training_session_dogs
     has_many :users, through: :dogs
 end
