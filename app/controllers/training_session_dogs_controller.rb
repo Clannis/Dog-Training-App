859b54d39d6aac66170c2adf7697aa3d81bc68af
Clannis <dodds1990@gmail.com>
2020-08-08 01:15:19 -0500

    remove debugging code

--- app/controllers/training_session_dogs_controller.rb
+++ app/controllers/training_session_dogs_controller.rb
@@ -3,7 +3,6 @@ class TrainingSessionDogsController < ApplicationController
     def destroy
         @training_session_dog = TrainingSessionDog.find(params[:id])
         @dog = @training_session_dog.dog
-        byebug
         @training_session_dog.delete
         redirect_to user_dog_path(@dog.user, @dog)
     end
