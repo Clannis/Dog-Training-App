6b6dec0f3d44febdd5b91a2d472576f5b0f18b57
Clannis <dodds1990@gmail.com>
2020-08-09 02:59:14 -0500

    validate uniqueness of trick by name

--- app/models/trick.rb
+++ app/models/trick.rb
@@ -2,6 +2,8 @@ class Trick < ApplicationRecord
     has_many :trick_courses
     has_many :courses, through: :trick_courses
 
+    validates :name, uniqueness: {case_sensitive: false}
+
     def trick_course(course)
         if self.trick_courses.count == 0
             nil
