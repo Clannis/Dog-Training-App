ad3abbd91de43922f04a2aafcadd5f3c7f8f755b
Clannis <dodds1990@gmail.com>
2020-08-13 16:46:20 -0500

    abstract logic away from course show page with helpers

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,20 @@
 module CoursesHelper
+
+    def course_edit_link
+        if session[:trainer_id]
+            link_to 'Edit this course', edit_course_path(@course)
+        end
+    end
+
+    def remove_trick_from_course_button(trick, course)
+        if session[:trainer_id]
+            button_to "Remove trick from Course", trick.trick_course(course) , method: :delete
+        end
+    end
+
+    def add_trick_to_to_course_link(course)
+        if session[:trainer_id]
+            link_to 'Add Trick to Course', course_tricks_path(course)
+        end
+    end
 end
