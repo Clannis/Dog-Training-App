4bbd0e4011b9fc6769c43cf67b9bf2a3010de4c1
Clannis <dodds1990@gmail.com>
2020-08-13 03:32:16 -0500

    add display of error messages for new and edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,13 @@ class UsersController < ApplicationController
     end
 
     def create
+        format_phone_number_input
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
-            redirect_to new_user_path
+            render 'new'
         end
     end
 
@@ -26,13 +27,22 @@ class UsersController < ApplicationController
     def update
         @user = current_user
         format_phone_number_input
-        @user.update(user_params)
-        redirect_to user_path(@user)
+        if @user.update(update_user_params)
+            redirect_to user_path(@user)
+        else
+            # cannot find why password error is added
+            @user.errors.delete(:password) if @user.errors[:password]   
+            render "edit"
+        end
     end
 
     private
 
     def user_params
         params.require(:user).permit(:username, :first_name, :last_name, :phone_number, :email, :password, :password_confirmation)
+    end
+
+    def update_user_params
+        params.require(:user).permit(:first_name, :last_name, :phone_number, :email)
     end
 end
