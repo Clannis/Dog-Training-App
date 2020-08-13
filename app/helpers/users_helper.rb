7d4db4558c813079d2d82d9f3cd3bd12376bed11
Clannis <dodds1990@gmail.com>
2020-08-13 16:20:35 -0500

    abstract away logic from user show page with helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+    def dog_link(user)
+        if user.dogs.count == 0
+            link_to 'Add Your First Dog', new_user_dog_path(user)
+        else
+            link_to "View Dogs", user_dogs_path(user)
+        end
+    end
 end
