538ee8fea07a3d5e922fce3e21cd0f1eb4ab837a
Clannis <dodds1990@gmail.com>
2020-08-13 22:04:46 -0500

    remove oauth and links to log in with facebook and google

--- Gemfile
+++ Gemfile
@@ -51,12 +51,6 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-
-
-gem "omniauth-facebook", "~> 6.0"
-
-gem "omniauth-google", "~> 1.0"
-
 gem "dotenv-rails", "~> 2.7"
 
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :developer unless Rails.env.production?
-    # provider :google, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
-    provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
     provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
   end
